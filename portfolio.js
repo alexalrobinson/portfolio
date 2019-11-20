2edbc44386f1e1375b6ddea1cf421f2d8aff4271
alexalrobinson <31484232+alexalrobinson@users.noreply.github.com>
2019-11-20 15:44:47 -0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,16 @@
+$(window).on('hashchange', function(e){
+    let hash = window.location.hash;
+    $("section").addClass("d-none");
+    $(hash).removeClass("d-none");
+});
+
 let themeClicks = 0;
 $('link[rel=stylesheet][href~="dark.css"]').attr('disabled', 'true');
 $('link[rel=stylesheet][href~="color.css"]').attr('disabled', 'true');
 
 $("#themeChange").click(function(){
     let factor = themeClicks % 3;
-    if(factor == 0){
+    if(factor === 0){
         $('link[rel=stylesheet][href~="light.css"]').attr('disabled', 'true');
         $('link[rel=stylesheet][href~="color.css"]').attr('disabled', 'true');
         $('link[rel=stylesheet][href~="dark.css"]').removeAttr('disabled');
