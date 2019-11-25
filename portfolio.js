0a980e16b77ae66cf84cbfbf83a4ddf8db4abba1
alexalrobinson <31484232+alexalrobinson@users.noreply.github.com>
2019-11-24 23:33:10 -0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,27 @@
-$(window).on('hashchange', function(e){
+function hashHandler(){
     let hash = window.location.hash;
-    $("section").addClass("d-none");
-    $(hash).removeClass("d-none");
+    $("section").addClass("hidden");
+    if(hash === ""){
+        $("#about").removeClass("hidden");
+    }
+    else {
+        $(hash).removeClass("hidden");
+    }
+}
+
+$(document).ready(()=>{
+    hashHandler();
+});
+
+$(window).on('hashchange', ()=>{
+    hashHandler();
 });
 
 let themeClicks = 0;
 $('link[rel=stylesheet][href~="dark.css"]').attr('disabled', 'true');
 $('link[rel=stylesheet][href~="color.css"]').attr('disabled', 'true');
 
-$("#themeChange").click(function(){
+$("#themeChange").click(()=>{
     let factor = themeClicks % 3;
     if(factor === 0){
         $('link[rel=stylesheet][href~="light.css"]').attr('disabled', 'true');
@@ -29,4 +42,14 @@ $("#themeChange").click(function(){
         $('#name').text("Light");
     }
     themeClicks++;
+});
+
+$(".navBTN").mouseover(function(){
+    $(this).children(".icon").addClass("hidden");
+    $(this).children(".icon-name").removeClass("hidden");
+});
+
+$(".navBTN").mouseout(function(){
+    $(this).children(".icon-name").addClass("hidden");
+    $(this).children(".icon").removeClass("hidden");
 });
