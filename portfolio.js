50b895fc6b3750f8fe6aee4e62243f8c6a7d1b3c
alexalrobinson <31484232+alexalrobinson@users.noreply.github.com>
2019-11-26 01:28:38 -0500

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,22 @@
+/*function hashHandler(){
+    let hash = window.location.hash;
+    $("section").addClass("hidden");
+    if(hash === ""){
+        $("#about").removeClass("hidden");
+    }
+    else {
+        $(hash).removeClass("hidden");
+    }
+}
+
+$(document).ready(()=>{
+    hashHandler();
+});
+
+$(window).on('hashchange', ()=>{
+    hashHandler();
+})*/
+
 let themeClicks = 0;
 $('link[rel=stylesheet][href~="dark.css"]').attr('disabled', 'true');
 $('link[rel=stylesheet][href~="color.css"]').attr('disabled', 'true');
@@ -8,19 +27,29 @@ $("#themeChange").click(()=>{
         $('link[rel=stylesheet][href~="light.css"]').attr('disabled', 'true');
         $('link[rel=stylesheet][href~="color.css"]').attr('disabled', 'true');
         $('link[rel=stylesheet][href~="dark.css"]').removeAttr('disabled');
-        $('#name').text("Dark");
+        $('#name').html(`<i class="fas fa-moon"></i>`);
     }
     else if(factor === 1){
         $('link[rel=stylesheet][href~="dark.css"]').attr('disabled', 'true');
         $('link[rel=stylesheet][href~="light.css"]').attr('disabled', 'true');
         $('link[rel=stylesheet][href~="color.css"]').removeAttr('disabled');
-        $('#name').text("Color");
+        $('#name').html(`<i class="fas fa-palette"></i>`);
     }
     else{
         $('link[rel=stylesheet][href~="dark.css"]').attr('disabled', 'true');
         $('link[rel=stylesheet][href~="color.css"]').attr('disabled', 'true');
         $('link[rel=stylesheet][href~="light.css"]').removeAttr('disabled');
-        $('#name').text("Light");
+        $('#name').html(`<i class="fas fa-sun"></i>`);
     }
     themeClicks++;
 });
+/*
+$(".navBTN").mouseover(function(){
+    $(this).children(".icon").addClass("hidden");
+    $(this).children(".icon-name").removeClass("hidden");
+});
+
+$(".navBTN").mouseout(function(){
+    $(this).children(".icon-name").addClass("hidden");
+    $(this).children(".icon").removeClass("hidden");
+});*/
