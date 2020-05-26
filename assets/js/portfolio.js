9527b1fe77bafed4bef3de1abf6489bcd10fbf4a
alexalrobinson <alexa.l.robinson@gmail.com>
2020-05-26 10:53:48 -0400

    reorganized

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -15,21 +15,21 @@ $('link[rel=stylesheet][href~="assets/css/color.css"]').attr('disabled', 'true')
 $("#themeChange").click(()=>{
     let factor = themeClicks % 3;
     if(factor === 0){
-        $('link[rel=stylesheet][href~="../css/light.css"]').attr('disabled', 'true');
-        $('link[rel=stylesheet][href~="../css/color.css"]').attr('disabled', 'true');
-        $('link[rel=stylesheet][href~="../css/dark.css"]').removeAttr('disabled');
+        $('link[rel=stylesheet][href~="assets/css/light.css"]').attr('disabled', 'true');
+        $('link[rel=stylesheet][href~="assets/css/color.css"]').attr('disabled', 'true');
+        $('link[rel=stylesheet][href~="assets/css/dark.css"]').removeAttr('disabled');
         $('#name').html(`<i class="fas fa-moon"></i>`);
     }
     else if(factor === 1){
-        $('link[rel=stylesheet][href~="../css/dark.css"]').attr('disabled', 'true');
-        $('link[rel=stylesheet][href~="../css/light.css"]').attr('disabled', 'true');
-        $('link[rel=stylesheet][href~="../css/color.css"]').removeAttr('disabled');
+        $('link[rel=stylesheet][href~="assets/css/dark.css"]').attr('disabled', 'true');
+        $('link[rel=stylesheet][href~="assets/css/light.css"]').attr('disabled', 'true');
+        $('link[rel=stylesheet][href~="assets/css/color.css"]').removeAttr('disabled');
         $('#name').html(`<i class="fas fa-palette"></i>`);
     }
     else{
-        $('link[rel=stylesheet][href~="../css/dark.css"]').attr('disabled', 'true');
-        $('link[rel=stylesheet][href~="../css/color.css"]').attr('disabled', 'true');
-        $('link[rel=stylesheet][href~="../css/light.css"]').removeAttr('disabled');
+        $('link[rel=stylesheet][href~="assets/css/dark.css"]').attr('disabled', 'true');
+        $('link[rel=stylesheet][href~="assets/css/color.css"]').attr('disabled', 'true');
+        $('link[rel=stylesheet][href~="assets/css/light.css"]').removeAttr('disabled');
         $('#name').html(`<i class="fas fa-sun"></i>`);
     }
     themeClicks++;
